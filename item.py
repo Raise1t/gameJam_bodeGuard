d099da61aa9fc1297892d1992f2f383f36f00abb
Dieu--Guillot Romain <romain.dieu--guillot@etu.univ-grenoble-alpes.fr>
2022-02-07 13:15:14 +0100

    classe mère item

---
+++ item.py
@@ -0,0 +1,11 @@
+class Item :
+    
+    def __init__(self, name, consumable) -> None:
+        self.__name = name
+        self.__consumable = consumable
+    
+    def getName():
+        return self.name
+    
+    def isConsumable():
+        return self.consumable
