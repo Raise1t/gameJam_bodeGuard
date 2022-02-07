2dfc051625620dde62d7cacee56ce0fba9281de5
gallerev <evan.galleron@etu.univ-grenoble-alpes.fr>
2022-02-07 17:24:21 +0100

    ajout de la tente et du perso

---
+++ tente.py
@@ -0,0 +1,9 @@
+import pygame
+from settings import *
+
+class Tente(pygame.sprite.Sprite):
+    def __init__(self,pos,groups):
+        super().__init__(groups)
+        self.image = pygame.image.load('texture/tente.png').convert_alpha()
+        pygame.transform.scale2x(self.image)
+        self.rect = self.image.get_rect(topleft = pos)
