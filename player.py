55da4b47f74acbcf690ebb50ad47c0f10e6e2dfc
Dieu--Guillot Romain <romain.dieu--guillot@etu.univ-grenoble-alpes.fr>
2022-02-07 13:08:40 +0100

    modification init

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 class player:
 
-    def __init__(self, name, skin):
+    def __init__(self, name, skin) -> None :
         self.__name = name
         self.__health = 100
         self.__food = 100
