f2551ebdf4552530273064ca4b8d962d4ae13512
Dieu--Guillot Romain <romain.dieu--guillot@etu.univ-grenoble-alpes.fr>
2022-02-07 12:49:50 +0100

    debut class joueur

---
+++ player.py
@@ -0,0 +1,8 @@
+class player:
+
+    def __init__(self, name, skin):
+        self.__name = name
+        self.__health = 100
+        self.__food = 100
+        self.__inventory = {}
+        self.__skin = ""
