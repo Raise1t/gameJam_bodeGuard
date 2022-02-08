71ff0b857a29e0ad35503ad027290e4ec2565edc
Dieu--Guillot Romain <romain.dieu--guillot@etu.univ-grenoble-alpes.fr>
2022-02-08 12:39:20 +0100

    getter and setter for plyer

--- player.py
+++ player.py
@@ -6,6 +6,7 @@
     #    self.__food = 100
     #    self.__inventory = {}
     #    self.__skin = ""
+from ast import If
 from curses import KEY_DOWN
 #from typing_extensions import Self
 import pygame
@@ -17,8 +18,32 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.image.load('texture/player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
         self.direction = pygame.math.Vector2()
-        self.speed = 5
+        self._speed = 5
         self.obstacle_sprites = obstacle_sprites
+        self._health = 100
+
+    #########################################################################
+
+    @property
+    def health(self):
+        return self._health
+    
+    @health.setter
+    def health(self, newHealth):
+        if newHealth < 0:
+            raise ValueError
+        else:
+            self._health = newHealth
+    
+    @property
+    def speed(self):
+        return self._speed
+    
+    @speed.setter
+    def speed(self, newSpeed):
+        self._speed = newSpeed
+
+    ###########################################################################
 
     def input(self):
         key = pygame.key.get_pressed()
@@ -62,8 +87,6 @@ class Player(pygame.sprite.Sprite):
                         self.rect.bottom = sprite.rect.top
                     if self.direction.y < 0:
                         self.rect.top = sprite.rect.bottom
-
-
 
     def update(self):
         self.input()
