f2888ffe6331fed62adf06ddf4cfae417168ccab
gallerev <evan.galleron@etu.univ-grenoble-alpes.fr>
2022-02-09 14:17:09 +0100

    cycle jour nuit, mais pas linverse

--- level.py
+++ level.py
@@ -4,10 +4,11 @@ import pygame
 from mob1 import Mob1
 from settings import *
 from water_block import Water_block
-from grass_block import Grass_block
-from wet_block import Wet_block
+from water_block import Water_block_night
+from grass_block import Grass_block, Grass_block_night
+from wet_block import Wet_block, Wet_block_night
 from player import Player
-from tente import Tente
+from tente import Tente, Tente_night
 from debug import debug
 from ui import UI
 import time
@@ -21,6 +22,8 @@ class Level:
         self.visible_sprites = YSortCameraGroup()
         self.obstacles_sprites = pygame.sprite.Group()
 
+        self.entity_list = []
+
         #get game screen space
         self.display_surface = pygame.display.get_surface()
 
@@ -33,6 +36,7 @@ class Level:
         # day n night
         self.day_duration = DAY_DURATION
         self.night_duration = NIGHT_DURATION
+        self.day_pass = False
 
     def create_map(self):
         for row_index,row in enumerate(MAP_1):
@@ -58,13 +62,48 @@ class Level:
                     
                 if col == ' ':
                     Grass_block((x,y),[self.visible_sprites])
-        Mob1((X_m,Y_m),[self.visible_sprites], self.obstacles_sprites)
+        self.entity_list.append(Mob1((X_m,Y_m),[self.visible_sprites], self.obstacles_sprites))
         Tente((X_t,Y_t),[self.visible_sprites, self.obstacles_sprites])
         self.player = Player((X_p,Y_p),[self.visible_sprites], self.obstacles_sprites)
+        self.entity_list.append(self.player)
+
+
+    def create_map_night(self):
+        for entity in self.entity_list:
+            self.visible_sprites.remove(entity)
+        for row_index,row in enumerate(MAP_1):
+            for col_index, col in enumerate(row):
+                x = col_index * BLOCKSIZE
+                y = row_index * BLOCKSIZE
+                if col == 'o':
+                    Water_block_night((x,y),[self.visible_sprites, self.obstacles_sprites])
+                if col == 'w':
+                    Wet_block_night((x,y),[self.visible_sprites])
+                if col == 'p':
+                    X_p = x
+                    Y_p = y
+                    Grass_block_night((x,y),[self.visible_sprites])
+                if col == 'm':
+                    X_m = x
+                    Y_m = y
+                    Grass_block_night((x,y),[self.visible_sprites])
+                if col == 't':
+                    Grass_block_night((x,y),[self.visible_sprites])
+                    X_t = x
+                    Y_t = y
+                if col == ' ':
+                    Grass_block_night((x,y),[self.visible_sprites])
+        Tente_night((X_t,Y_t),[self.visible_sprites, self.obstacles_sprites])
+        for entity in self.entity_list:
+            self.visible_sprites.add(entity)
+
 
     def run(self):
         #draw things
         #self.visible_sprites.draw(self.display_surface)
+        if pygame.time.get_ticks() >= DAY_DURATION and not self.day_pass:
+            self.day_pass = True
+            self.create_map_night()
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
         self.visible_sprites.enemy_update(self.player)
